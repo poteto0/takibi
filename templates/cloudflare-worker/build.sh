895ce80519312baf2c686d451badd90f388ffa38
poteto0 <mahiro.fukuda.aries@gmail.com>
2026-06-02 17:03:27 +0900

    fix(template): use templ generate directly in build.sh

--- templates/cloudflare-worker/build.sh
+++ templates/cloudflare-worker/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
-go tool templ generate
+templ generate
 go run github.com/syumai/workers/cmd/workers-assets-gen -mode=go
 GOOS=js GOARCH=wasm go build -ldflags="-s -w" -o ./build/app.wasm .
